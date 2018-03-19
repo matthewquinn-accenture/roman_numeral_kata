226609ff3fd2eaa0e12b1f8459b3df573cbfe12b
matthew quinn <matthewquinn@matthews-air.attlocal.net>
2018-03-19 15:27:13 -0400

    apples

--- ci/resources/ci-test.sh
+++ ci/resources/ci-test.sh
@@ -1,4 +1,13 @@
 #!/bin/bash -e
 
+echo "installing yarn items"
+sudo apt-get update && sudo apt-get install yarn
+
+echo "instaling node"
+sudo apt-get install npm
+
+echo "installing yarn"
+yarn install
+
 echo "running tests"
 npm test
