ea1c7d6c88d1b0d5f6830be9789a804433ff6507
matthew quinn <matthewquinn@matthews-air.attlocal.net>
2018-03-19 22:02:07 -0400

    apples

--- ci/resources/ci-test.sh
+++ ci/resources/ci-test.sh
@@ -1,14 +1,5 @@
 #!/bin/bash -e
 
-echo "updating and adding sudo"
-apt-get update && apt-get install -y sudo
-
-echo "installing yarn items"
-sudo apt-get update && sudo apt-get install yarn
-
-echo "instaling node"
-sudo apt-get install npm
-
 echo "installing yarn"
 yarn install
 
